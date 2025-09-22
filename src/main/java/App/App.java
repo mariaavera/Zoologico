d891cfe2f83ea0f1c8ab5d5d1de107fbe00fbb16
mariaavera <marialevp240208@gmail.com>
2025-09-21 21:08:22 -0500

    .

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -16,8 +16,8 @@ public class App {
         Animal perro = new Perro("Tobby", (byte)10, "Poodle", "Blanco", "1234");
         Animal gato = new Gato("Pluto",(byte)2,"Criollo", "verde", "109491" );
         Animal pollo = new Pollo("Pollito",(byte)4,"Amarillo","8756");
-        Animal leon = new Leon("Leonidas",(byte)5, (double)114.5,"Sabana","23435");
-        Animal cerdo = new Cerdo("Puerqui",(byte)1,(double)12,"2354");
+        Animal leon = new Leon("Leonidas",(byte)5, 114.5,"Sabana","23435");
+        Animal cerdo = new Cerdo("Puerqui",(byte)1, 12,"2354");
 
         zoologico.registrarAnimal(perro);
         zoologico.registrarAnimal(gato);
